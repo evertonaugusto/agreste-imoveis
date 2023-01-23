2cdc8d16286aca1e4bd085219e845ea555f1ad36
evertonaugusto <evertonaugusto1997@gmail.com>
2023-01-23 01:27:47 -0300

    added imoveis section to index page

--- resources/views/painel/index.blade.php
+++ resources/views/painel/index.blade.php
@@ -49,7 +49,26 @@
             </div>
         </div>
     </div>
-    <div>
-        teste
+    <div class="bg-orange-500 py-10 px-10">
+        <div class="bg-gray-800 h-20 relative">
+            <span class="absolute -top-8 text-white font-bold text-5xl">Imovéis</span>
+        </div>
+        <div class="mt-10 flex flex-row">
+            @foreach ($imoveis as $imovel)
+                <div class="p-2 w-full sm:w-1/2 lg:w-1/3">
+                    <div class="bg-white overflow-hidden shadow-lg flex flex-col w-full">
+                        <div class="w-full h-52" style="background-image: url('https://fotos.vivadecora.com.br/piscina-casa-cristal-design-de-interiores-iara-kilaris-iarakilaris-290706-square_cover_xsmall.jpg')"></div>
+                        <div class="p-4 relative">
+                            <div class="bg-gray-700 rounded-sm font-mono font-bold text-lg text-white absolute right-5 -mt-8 px-2 py-0.5 -rotate-3">aluga-se</div>
+                            <span class="bg-orange-700 px-3 text-white text-sm rounded-full">{{ $imovel->tipo_imovel }}</span>
+                            <p class="mt-2">{{ $imovel->descricao }}</p>
+                            <p class="bg-gray-200 px-2 py-1 mt-2 font-bold text-xl text-center rounded-md">R$ {{ $imovel->valor }} / mês</p>
+                            <p class="mt-2"><span class="font-bold">Extensão:</span> {{ $imovel->metros_quadrados }} m²</p>
+                            <a href="{{ route('imoveis.show', [$imovel->id]) }}"><p class="bg-green-700 w-28 float-right py-1 mt-8 font-bold text-center text-white text-base rounded-full">VER MAIS</p></a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
